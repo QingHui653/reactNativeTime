12816f89f22e4949d725ae0d2ba21f96804dc665
qinghui <910944453@qq.com>
2018-10-13 12:27:21 +0800

    reactnative 相机 二维码 扫描

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Main from './views/Main';
 import Index from './views/Index';
+import CameraDemo from './views/CameraDemo';
 import {name as appName} from './app.json';
 
 import Routers from './views/example/Routers';
 
 
-AppRegistry.registerComponent(appName, () => Index);
+AppRegistry.registerComponent(appName, () => CameraDemo);
